a286155987ec626ef4f78c72d649b37650cb821c
keenwon <semanwmj@gmail.com>
2023-04-27 20:07:41 +0800

    fix: ts 类型

--- typings/modules.d.ts
+++ typings/modules.d.ts
@@ -1,15 +1,4 @@
 declare module '*.css'
-declare module '*.svg'
-declare module '*.pdf'
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.jpeg'
-declare module '*.gif'
-declare module '*.bmp'
-declare module '*.ico'
-declare module '*.md'
-declare module '*.mdx'
-declare module '*?truncate'
 
 declare module '*.less' {
   interface Less {
